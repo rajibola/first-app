ea52bc6314b6a11b6223a0710586a5bca47538e7
ridwan <rajibola1997@gmail.com>
2019-12-17 14:28:22 -0800

    adding new project

---
+++ precache-manifest.f25bbb0f47ef0926d5ce7e54bb6fda62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee8078587ef61a614fbc58d14c48e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d95c0229fb09cb988e8a",
+    "url": "/static/css/main.7372a935.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d95c0229fb09cb988e8a",
+    "url": "/static/js/main.3395650b.chunk.js"
+  },
+  {
+    "revision": "01e5a42a0a779b392583",
+    "url": "/static/js/runtime-main.58c14e21.js"
+  }
+]);
